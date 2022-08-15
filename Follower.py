6014188ca829e87ffa69b64bb33c4963e90fa584
Camonophy <pflug-oliver@gmx.de>
2022-08-15 16:13:53 +0200

    Fix critical file bloating

--- Follower.py
+++ Follower.py
@@ -31,49 +31,105 @@ def follow():
     with open("Follower.txt", 'r') as file:
         usernames = file.read().split("\n")
 
-    with open("Follower.txt", 'w') as file:
         while usernames:
-            user = usernames.pop(0)
-            wait = random.randint(300, 900)
-            try:
-                api.create_friendship(screen_name=user)
-                print("Added {}".format(user))
-            except:
-                print("Failed")
-                wait = 100
+            with open("Follower.txt", 'w') as file:
+                user = usernames.pop(0)
+                wait = random.randint(300, 900)
+                try:
+                    api.create_friendship(screen_name=user)
+                    print("Added {}".format(user))
+                except:
+                    print("Failed")
+                    wait = 100
 
-            file.write("\n".join(usernames))
-            time.sleep(wait)
+                file.write("\n".join(usernames))
+                time.sleep(wait)
 
-def createList():
-    index = 1
-    follower = []
+    def createList():
+        index = 1
+        follower = []
 
-    while 1:
-        os.system("wget https://www.letsallfollowback.com/?p={} -O html.txt".format(index))
-        html = ""
+        while 1:
+            os.system("wget https://www.letsallfollowback.com/?p={} -O html.txt".format(index))
+            html = ""
 
-        with open("html.txt", 'r') as file:
-            html = file.read().split("\n")
+            with open("html.txt", 'r') as file:
+                html = file.read().split("\n")
 
-        potentialFollower = []
+            potentialFollower = []
 
-        for line in html:
-            if "BLANK" in line and "retweet" not in line:
-                potentialFollower.append(line.split(">")[1].split("<")[0])
+            for line in html:
+                if "BLANK" in line and "retweet" not in line:
+                    potentialFollower.append(line.split(">")[1].split("<")[0])
 
-        potentialFollower.remove("--")
+            potentialFollower.remove("--")
 
-        if len(potentialFollower) < 10:
-            break
-        else:
-            index += 1
-            follower += potentialFollower
+            if len(potentialFollower) < 10:
+                break
+            else:
+                index += 1
+                follower += potentialFollower
 
-    os.system("touch Follower.txt")
-    with open("Follower.txt", 'w') as file:
-        for f in follower:
-            file.write(f + "\n")
+        os.system("touch Follower.txt")
+        with open("Follower.txt", 'w') as file:
+            for f in follower:
+                file.write(f + "\n")
+        os.system("cp Follower.txt FollowerVanilla.txt")
+
+    def updateList():
+        index = 1
+        updatedContent = ""
+        follower = []
+        prevPotentialFollower = []
+
+        # Get current potential follower from the website
+        while 1:
+            os.system("wget https://www.letsallfollowback.com/?p={} -O html.txt".format(index))
+            html = ""
+
+            with open("html.txt", 'r') as file:
+                html = file.read().split("\n")
+
+            potentialFollower = []
+
+            for line in html:
+                if "BLANK" in line and "retweet" not in line:
+                    potentialFollower.append(line.split(">")[1].split("<")[0])
+
+            potentialFollower.remove("--")
+
+            if len(potentialFollower) < 10:
+                break
+            else:
+                index += 1
+                follower += potentialFollower
+
+        # Load the previous potential follower list
+        try:
+            with open("FollowerVanilla.txt", 'r') as file:
+                prevPotentialFollower = file.read().split("\n")
+        except:
+            pass
+
+        print(len(prevPotentialFollower))
+        print(len(follower))
+
+        # Romove any duplicate
+        follower = filter(lambda i: i not in prevPotentialFollower, follower)
+        print(list(follower))
+
+        try:
+            with open("Follower.txt", "r") as file:
+                updatedContent = file.read()
+        except:
+            pass
+
+        updatedContent = "\n".join(follower) + "\n" + updatedContent
+
+        # Set the newly generated Follower file as the new Vanilla file
+        with open("Follower.txt", "w") as file:
+            file.write(updatedContent)
+        os.system("cp Follower.txt FollowerVanilla.txt")
 
 '''
 # Get credentials
